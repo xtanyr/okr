03154d68680e354beb77b2c989046716c3551fd6
xtanyr <xtanyr@gmail.com>
2025-08-29 03:27:11 +0600

    211

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,9 +7,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth': 'http://localhost:4000',
-      '/user': 'http://localhost:4000',
-      '/okr': 'http://localhost:4000',
+      '/auth': 'http://158.160.136.51:4000',
+      '/user': 'http://158.160.136.51:4000',
+      '/okr': 'http://158.160.136.51:4000',
     },
   },
   build: {
