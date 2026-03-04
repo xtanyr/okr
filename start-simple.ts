d002372a9445ff51df340e34f7eccde79e07a377
OKR User <xtanyr@gmail.com>
2026-03-04 14:20:13 +0600

    2route

--- start-simple.ts
+++ start-simple.ts
@@ -39,7 +39,17 @@ const corsOptions = {
 
 // Backend API Middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.status(200).send();
+  } else {
+    next();
+  }
+});
 app.use(express.json());
 
 // API Routes - MUST come before static file serving
